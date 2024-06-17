6ab8e1b785683015b387a0e2e880631795eecadf
Pranesh Bala Murugan M <96689686+prana8799@users.noreply.github.com>
2024-06-17 12:39:26 +0530

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,1 +1,50 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+public class TestNGSeleniumDemo {
+	
+	WebDriver driver;
+	
+	
+	@BeforeClass
+	public void setUp() {
+		System.setProperty("Webdriver.gecko.driver", "D:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		
+		
+		driver=new EdgeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void login() throws InterruptedException {
+		driver.findElement(By.cssSelector("#email")).sendKeys("praneshbalamurugan@gmail.com");
+		
+		Thread.sleep(2000L);
+		
+		driver.findElement(By.id("pass")).sendKeys("jhk998");
+		
+		Thread.sleep(2000L);;
+		
+		driver.findElement(By.xpath("//button[value='1']")).click();
+		
+	}
+		
+		
+		@AfterClass
+		public void tearDown() {
+			if(driver!=null) {
+				driver.close();
+			}
+		
+	}
+
+	
+		
+}
